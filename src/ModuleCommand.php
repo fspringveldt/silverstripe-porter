c22d87190d6017d406ecd48935b707a6ab21ad7e
Franco Springveldt <franco@silverstripe.com>
2018-02-16 14:13:25 +1300

    Initial commit

---
+++ src/ModuleCommand.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace SilverStripe\Porter;
+
+use GuzzleHttp\Client;
+use Symfony\Component\Process\Process;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Filesystem\Exception\IOExceptionInterface;
+
+/**
+ * Class ModuleCommand
+ */
+class ModuleCommand extends Command
+{
+
+}
