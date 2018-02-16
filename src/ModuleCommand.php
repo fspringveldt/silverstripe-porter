9e42308810569d5cde5f7d24a46fdde7f247679d
Franco Springveldt <franco@silverstripe.com>
2018-02-16 23:50:01 +1300

    Added extra validation for escaping namespaces
    Signed-off-by: Franco Springveldt <franco@silverstripe.com>

--- src/ModuleCommand.php
+++ src/ModuleCommand.php
@@ -100,6 +100,14 @@ class ModuleCommand extends Command
         $this->moduleName = $input->getArgument(self::ARGUMENTS_MODULE_NAME);
         $this->namespace = $input->getArgument(self::ARGUMENTS_MODULE_NAMESPACE);
         $this->moduleType = 'silverstripe-module';
+
+        if (in_array(substr_count($this->namespace, '\\'), [0, 1])) {
+            $message = "It seems your namespace is formed incorrectly.\n"
+                . "Possible examples are NameSpace\\\\ or NameSpace\\\\Folder\\\\\n"
+                . "[Double backslashes]";
+            throw new RuntimeException($message);
+        }
+
         if (stripos($this->moduleName, DIRECTORY_SEPARATOR) === false) {
             throw new RuntimeException('Invalid module name given. Use the format module/name');
         }
